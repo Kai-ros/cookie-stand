b73ec6b658a00e9f31b8097a0626fe77e1262916
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-06-10 15:12:59 -0700

    Added readme file.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 'use strict';
 
 // object literals for each shop location
+// 
 // need to store:
-// min customers
-// max customers
-// avg customers
-// method math.random
-// results in array
+// min hourly customers
+// max hourly customers
+// calculate avg cookies per customer per hour
+// math.random method applied to ObjLit to derive customers per hr
+// results for each location in a separate array, possibly as a property of that given location Obj
+// 
+// Display the values of each array as <ul> in browser
 
 // hours of operation: 6a-8p
 
