0d98cdf39fc304c5495f847087db111d0d1c5e75
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-06-10 14:53:27 -0700

    Changed a file name.

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// object literals for each shop location
+// need to store:
+// min customers
+// max customers
+// avg customers
+// method math.random
+// results in array
+
+// hours of operation: 6a-8p
+
+// add/remove locations from the daily report
