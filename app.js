33d122a81ee1f21b0bfbe8eb0577727762348513
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-06-11 16:37:40 -0700

    Finished refactors to include constructors and functions from yesterday's code.

--- app.js
+++ app.js
@@ -2,132 +2,53 @@
 
 // add/remove locations from the daily report
 
-// TODO: add constructors, move DOM manipulations into functions, transform the lists into tables
+// TODO:  transform the lists into tables, render methods for tables on constructor(wrap the for loop)
 // TODO: Strech goals - total all of the stores, totals per hour per store
 
-// Object literals for each store location
+// Object Literal constructor function for each store location
 
-var firstAndPikeStore = {
-  storeLocation : '1st and Pike',
-  minimumCustomersPerHour : 23,
-  maximumCustomersPerHour : 65,
-  averageCookiePerCustomer : 6.3,
-  openingHour : 6,
-  closingHour : 20,
-  hourlyCookiesPurchasedArray : [],
-  totalCookiesSold : 0,
-  calculateRandomCustomersPerHour: function() {
-    return this.minimumCustomersPerHour + (Math.floor(Math.random() * (this.maximumCustomersPerHour - this.minimumCustomersPerHour + 1)));
-  },
-  calculateAverageCookiesPerCustomerPerHour: function() {
-    for (var i = this.openingHour; i < this.closingHour; i++) {
-      var averageCookiesPerHour = Math.floor(this.averageCookiePerCustomer * this.calculateRandomCustomersPerHour());
+var storeLocationSection = document.getElementById('store-Location-Hourly-Totals');
 
-      this.totalCookiesSold += averageCookiesPerHour;
+var StoreLocation = function(
+  storeLocation, 
+  minimumCustomersPerHour, 
+  maximumCustomersPerHour, 
+  averageCookiePerCustomer,
+  openingHour,
+  closingHour,
+  hourlyCookiesPurchasedArray,
+  totalCookiesSold ) {
+  this.storeLocation = storeLocation;
+  this.minimumCustomersPerHour = minimumCustomersPerHour;
+  this.maximumCustomersPerHour = maximumCustomersPerHour;
+  this.averageCookiePerCustomer = averageCookiePerCustomer;
+  this.openingHour = openingHour;
+  this.closingHour = closingHour;
+  this.hourlyCookiesPurchasedArray = hourlyCookiesPurchasedArray;
+  this.totalCookiesSold = totalCookiesSold;
+};
+StoreLocation.prototype.calculateRandomCustomersPerHour = function() {
+  return this.minimumCustomersPerHour + (Math.floor(Math.random() * (this.maximumCustomersPerHour - this.minimumCustomersPerHour + 1)));
+};
+StoreLocation.prototype.calculateAverageCookiesPerCustomerPerHour = function() {
+  for (var i = this.openingHour; i < this.closingHour; i++) {
+    var averageCookiesPerHour = Math.floor(this.averageCookiePerCustomer * this.calculateRandomCustomersPerHour());
 
-      this.hourlyCookiesPurchasedArray.push(i + '00 - ' + (i + 1) + '00 : ' + averageCookiesPerHour + ' cookies');
-    }
-    return this.hourlyCookiesPurchasedArray;
+    this.totalCookiesSold += averageCookiesPerHour;
+
+    this.hourlyCookiesPurchasedArray.push(i + '00 - ' + (i + 1) + '00 : ' + averageCookiesPerHour + ' cookies');
   }
+  return this.hourlyCookiesPurchasedArray;
 };
 
-var seaTacAirport = {
-  storeLocation : 'SeaTac Airport',
-  minimumCustomersPerHour : 3,
-  maximumCustomersPerHour : 24,
-  averageCookiePerCustomer : 1.2,
-  openingHour : 6,
-  closingHour : 20,
-  hourlyCookiesPurchasedArray : [],
-  totalCookiesSold : 0,
-  calculateRandomCustomersPerHour: function() {
-    return this.minimumCustomersPerHour + (Math.floor(Math.random() * (this.maximumCustomersPerHour - this.minimumCustomersPerHour + 1)));
-  },
-  calculateAverageCookiesPerCustomerPerHour: function() {
-    for (var i = this.openingHour; i < this.closingHour; i++) {
-      var averageCookiesPerHour = Math.floor(this.averageCookiePerCustomer * this.calculateRandomCustomersPerHour());
+var firstAndPikeStore = new StoreLocation('1st and Pike', 23, 65, 6.3, 6, 20, [], 0);
+var seaTacAirport = new StoreLocation('SeaTac Airport', 3, 24, 1.2, 6, 20, [], 0);
+var seattleCenter = new StoreLocation('Seattle Center', 11, 38, 3.7, 6, 20, [], 0);
+var capitolHill = new StoreLocation('Capitol Hill',20, 38, 2.3, 6, 20, [], 0);
+var alki = new StoreLocation('Alki', 2, 16, 4.6, 6, 20, [], 0);
 
-      this.totalCookiesSold += averageCookiesPerHour;
-
-      this.hourlyCookiesPurchasedArray.push(i + '00 - ' + (i + 1) + '00 : ' + averageCookiesPerHour + ' cookies');
-    }
-    return this.hourlyCookiesPurchasedArray;
-  }
-};
-
-var seattleCenter = {
-  storeLocation : 'Seattle Center',
-  minimumCustomersPerHour : 11,
-  maximumCustomersPerHour : 38,
-  averageCookiePerCustomer : 3.7,
-  openingHour : 6,
-  closingHour : 20,
-  hourlyCookiesPurchasedArray : [],
-  totalCookiesSold : 0,
-  calculateRandomCustomersPerHour: function() {
-    return this.minimumCustomersPerHour + (Math.floor(Math.random() * (this.maximumCustomersPerHour - this.minimumCustomersPerHour + 1)));
-  },
-  calculateAverageCookiesPerCustomerPerHour: function() {
-    for (var i = this.openingHour; i < this.closingHour; i++) {
-      var averageCookiesPerHour = Math.floor(this.averageCookiePerCustomer * this.calculateRandomCustomersPerHour());
-
-      this.totalCookiesSold += averageCookiesPerHour;
-
-      this.hourlyCookiesPurchasedArray.push(i + '00 - ' + (i + 1) + '00 : ' + averageCookiesPerHour + ' cookies');
-    }
-    return this.hourlyCookiesPurchasedArray;
-  }
-};
-
-var capitolHill = {
-  storeLocation : 'Capitol Hill',
-  minimumCustomersPerHour : 20,
-  maximumCustomersPerHour : 38,
-  averageCookiePerCustomer : 2.3,
-  openingHour : 6,
-  closingHour : 20,
-  hourlyCookiesPurchasedArray : [],
-  totalCookiesSold : 0,
-  calculateRandomCustomersPerHour: function() {
-    return this.minimumCustomersPerHour + (Math.floor(Math.random() * (this.maximumCustomersPerHour - this.minimumCustomersPerHour + 1)));
-  },
-  calculateAverageCookiesPerCustomerPerHour: function() {
-    for (var i = this.openingHour; i < this.closingHour; i++) {
-      var averageCookiesPerHour = Math.floor(this.averageCookiePerCustomer * this.calculateRandomCustomersPerHour());
-
-      this.totalCookiesSold += averageCookiesPerHour;
-
-      this.hourlyCookiesPurchasedArray.push(i + '00 - ' + (i + 1) + '00 : ' + averageCookiesPerHour + ' cookies');
-    }
-    return this.hourlyCookiesPurchasedArray;
-  }
-};
-
-var alki = {
-  storeLocation : 'Alki',
-  minimumCustomersPerHour : 2,
-  maximumCustomersPerHour : 16,
-  averageCookiePerCustomer : 4.6,
-  openingHour : 6,
-  closingHour : 20,
-  hourlyCookiesPurchasedArray : [],
-  totalCookiesSold : 0,
-  calculateRandomCustomersPerHour: function() {
-    return this.minimumCustomersPerHour + (Math.floor(Math.random() * (this.maximumCustomersPerHour - this.minimumCustomersPerHour + 1)));
-  },
-  calculateAverageCookiesPerCustomerPerHour: function() {
-    for (var i = this.openingHour; i < this.closingHour; i++) {
-      var averageCookiesPerHour = Math.floor(this.averageCookiePerCustomer * this.calculateRandomCustomersPerHour());
-
-      this.totalCookiesSold += averageCookiesPerHour;
-
-      this.hourlyCookiesPurchasedArray.push(i + '00 - ' + (i + 1) + '00 : ' + averageCookiesPerHour + ' cookies');
-    }
-    return this.hourlyCookiesPurchasedArray;
-  }
-};
-
-// Call individual object literal functions to generate data for lists.
+// Call individual object literal functions to generate data for lists. 
+// In other words turns on the JS functionality.
 firstAndPikeStore.calculateAverageCookiesPerCustomerPerHour();
 seaTacAirport.calculateAverageCookiesPerCustomerPerHour();
 seattleCenter.calculateAverageCookiesPerCustomerPerHour();
@@ -149,44 +70,79 @@ var storeLocationObjectLiteralsArray = [
 // Fill the elem with values.
 // Add the new elem to the page.
 
-
-// Nested for loop to iterate over the arrays/objects and pull out what is necessary sequentially and populate it to the screen.
-// In other words, complete and utter DOM mastery.
-for (var i = 0; i < storeLocationObjectLiteralsArray.length; i++) {
-  // In each loop I need to access the ObjLit's and generate details
-
-  // Store current Object Literal in a temporary variable
-  var currentStoreLocationObjectLiteral = storeLocationObjectLiteralsArray[i];
-
-  // Store location of list entries
-  var storeLocationSection = document.getElementById('store-Location-Hourly-Totals');
+// Function to create <h2> title element.
+var storeLocationH2ELems = function(singleStoreObjectLiteral) {
   var storeLocationHeaderElem = document.createElement('h2');
-  storeLocationHeaderElem.textContent = currentStoreLocationObjectLiteral.storeLocation;
+  storeLocationHeaderElem.textContent = singleStoreObjectLiteral.storeLocation;
   storeLocationSection.appendChild(storeLocationHeaderElem);
 
-  // Header for list entries
+  return;
+};
+
+// Function to create <h3> title element.
+var cookiesPurchasedH3Elems = function() {
   var headerForListEntries = document.createElement('h3');
   headerForListEntries.textContent = 'Cookies Purchased Per Hour';
   storeLocationSection.appendChild(headerForListEntries);
 
-  // Add empty <ul> elem.
+  return;
+};
+
+// Function to create empty <ul> element.
+var emptyULElems = function() {
   var parentULElemForListEntries = document.createElement('ul');
   storeLocationSection.appendChild(parentULElemForListEntries);
+
+  return;
+};
+
+// Function to create generic <li> element list entry.
+var generateLIElems = function(message) {
+  var childLIElemForListEntries = document.createElement('li');
+  childLIElemForListEntries.textContent = message;
+  storeLocationSection.appendChild(childLIElemForListEntries);
+
+  return;
+};
+
+// Function to fill <li> element with hourly cookies sold.
+var hourlyCookiesSoldLIElems = function(singleStoreObjectLiteral) {
+  var hourlyCookiesSoldMessage = singleStoreObjectLiteral.hourlyCookiesPurchasedArray[j];
+  generateLIElems(hourlyCookiesSoldMessage);
+
+  return;
+};
+
+// Function to fill <li> element with total cookies sold.
+var totalCookiesSoldLIElems = function(singleStoreObjectLiteral) {
+  var totalCookiesSoldMessage = 'Total cookies sold: ' + singleStoreObjectLiteral.totalCookiesSold;
+  generateLIElems(totalCookiesSoldMessage);
+
+  return;
+};
+
+// Nested for loop to iterate over the arrays/objects and pull out what is necessary sequentially and populate it to the screen.
+// In other words, complete and utter DOM mastery.
+for (var i = 0; i < storeLocationObjectLiteralsArray.length; i++) {
+
+  // Store current Object Literal in a temporary variable
+  var currentStoreLocationObjectLiteral = storeLocationObjectLiteralsArray[i];
+
+  storeLocationH2ELems(currentStoreLocationObjectLiteral);
+  
+  cookiesPurchasedH3Elems();
+  
+  emptyULElems();
 
   // For loop to iterate over each Object Literal's individual hourly cookies array.
   for (var j = 0; j < currentStoreLocationObjectLiteral.hourlyCookiesPurchasedArray.length; j++) {
 
     // Add <li> entries from the hourly cookies array.
-    var childLIElemForListEntries = document.createElement('li');
-    childLIElemForListEntries.textContent = currentStoreLocationObjectLiteral.hourlyCookiesPurchasedArray[j];
-    storeLocationSection.appendChild(childLIElemForListEntries);
+    hourlyCookiesSoldLIElems(currentStoreLocationObjectLiteral);
 
     // Within the second for loop a conditional to launch a final result of total
     if (j === (currentStoreLocationObjectLiteral.hourlyCookiesPurchasedArray.length - 1)) {
-      var totalCookiesSoldElemForEachStore = document.createElement('li');
-      var totalCookiesSoldMessage = 'Total cookies sold: ' + currentStoreLocationObjectLiteral.totalCookiesSold;
-      totalCookiesSoldElemForEachStore.textContent = totalCookiesSoldMessage;
-      storeLocationSection.appendChild(totalCookiesSoldElemForEachStore);
+      totalCookiesSoldLIElems(currentStoreLocationObjectLiteral);
     }
   }
 
